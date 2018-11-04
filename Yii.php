efe3eb199681c1bab309d5245687130fdfa23f43
vantruong1898 <vantruong1898@gmail.com>
2018-11-05 01:12:02 +0700

    Update

--- Yii.php
+++ Yii.php
@@ -3,11 +3,15 @@
 include_once __DIR__ . '/Common.php';
 require_once __DIR__ . '/BaseYii.php';
 
-class Yii extends \izi\BaseYii
+class Yii extends \yii\BaseYii
 {
     
-    public static $app;
+    public static $device = 'desktop', $is_mobile = false,$_category = [],$site = [],$item = [];
     
      
     
 }
+
+spl_autoload_register(['Yii', 'autoload'], true, true);
+Yii::$classMap = require __DIR__ . '/classes.php';
+Yii::$container = new yii\di\Container();
